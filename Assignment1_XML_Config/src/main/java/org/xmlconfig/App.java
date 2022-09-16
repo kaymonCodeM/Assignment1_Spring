971306274ff36a79bb67e6a5d673c553bba03f79
Kaymon McCain <kaymonCodeM@outlook.com>
2022-09-16 12:00:14 -0500

    XML config complete

--- Assignment1_XML_Config/src/main/java/org/xmlconfig/App.java
+++ Assignment1_XML_Config/src/main/java/org/xmlconfig/App.java
@@ -1,5 +1,8 @@
 package org.xmlconfig;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student);
     }
 }
